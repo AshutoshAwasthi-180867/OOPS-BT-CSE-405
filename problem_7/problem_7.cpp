be20cd203275d1e82579a6285a57b698536dfd6e
Ashutosh <ashuawasthi2078@gmail.com>
2020-02-10 15:46:36 +0530

    problem_7

--- problem_7/problem_7.cpp
+++ problem_7/problem_7.cpp
@@ -1,3 +1,4 @@
+/*   program to print system time after 15 minutes from current system time using friend function*/
 #include<iostream>
 #include<ctime>
 using namespace std;
@@ -12,18 +13,22 @@ class Time{
         }    
         void display()
         {
-            cout<<"\n hour --"<<hours;
+            cout<<"time after 15 minutes"<<endl;
+            cout<<" hour --"<<hours;
             cout<<"\n min --"<<mins;
         }
         friend void sum(Time&);
 };
 void sum(Time& x)
 {
-    time_t now =time(0);
-    tm *ltm2;
-    ltm2=localtime(&(x.now));
-    x.hours=ltm2->tm_hour+x.ltm1->tm_hour;
-    x.mins=ltm2->tm_min+x.ltm1->tm_min;
+    //time_t now =time(0);
+    //tm *ltm2;
+    //ltm2=localtime(&(x.now));
+    int t,r;
+    t=(x.ltm1->tm_min+15)/60;
+    r=(x.ltm1->tm_min+15)%60;
+    x.hours=t+x.ltm1->tm_hour;
+    x.mins=r;
 }
 int main()
 {
